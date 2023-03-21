139001660f58386ddb089226888de3656772752a
Yaroslav Korin <lexuslx570vstoyota@gmail.com>
2023-03-21 19:24:40 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryCreate = document.querySelector('.gallery');
+
+const galleryEl = galleryItems.map(({ preview, original, description }) =>
+    `<div class = "gallery__item">
+<a class = "gallery__link" href = "${original}">
+<img class = "gallery__img" src ="${preview}" alt = "${description}" />
+</a>
+</div>`
+).join('');
