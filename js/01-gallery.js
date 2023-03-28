a4055529757deeecfc48a1e1ad0cb8b0dc94952a
Yaroslav Korin <lexuslx570vstoyota@gmail.com>
2023-03-28 13:24:44 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,7 +8,7 @@ const galleryCreate = document.querySelector('.gallery');
 const galleryEl = galleryItems.map(({ preview, description, original }) =>
     `<div class = "gallery__item">
 <a class = "gallery__link" href = "${original}">
-<img class = "gallery__img" src ="${preview}" data-source="${original}" alt = "${description}" />
+<img class = "gallery__image" src ="${preview}" data-source="${original}" alt = "${description}" />
 </a>
 </div>`
 ).join('');
